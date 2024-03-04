ed8d52d2cab21490337f1ceb012bba8c65cf4661
1nchy <2493242629@qq.com>
2024-03-04 10:03:45 +0800

    feat(command): new structure to store commands

---
+++ command.cpp
@@ -0,0 +1,60 @@
+#include "command.hpp"
+
+namespace asp {
+void command_sequence::build_word(const std::string& _s) {
+    if (_s.empty()) return;
+    _further_input = false;
+    if (_in_ready) {
+        _parsed_command.back()._redirect_in = _s;
+        _in_ready = false;
+    }
+    else if (_out_ready) {
+        _parsed_command.back()._redirect_out = _s;
+        _out_ready = false;
+    }
+    else {
+        if (_new_cmd) {
+            _parsed_command.emplace_back();
+        }
+        if (_new_ins) {
+            _parsed_command.back().create_instruction();
+        }
+        _parsed_command.back().append_word(_s);
+        _new_cmd = false; _new_ins = false;
+    }
+}
+void command_sequence::build_instruction() {
+    if (_new_ins) {
+        return;
+    }
+    _new_ins = true;
+}
+void command_sequence::build_command(const std::string& _relation, bool _bg) {
+    if (_new_cmd) {
+        return;
+    }
+    _parsed_command.back()._background = _bg;
+    _command_relation.emplace_back(_relation);
+    _new_cmd = true; _new_ins = true;
+}
+void command_sequence::prepare_redirection(const std::string& _redir) {
+    if (_redir == "<") {
+        _in_ready = true;
+    }
+    else if (_redir == ">") {
+        _out_ready = true;
+        _parsed_command.back()._redirect_out_type = 1;
+    }
+    else if (_redir == ">>") {
+        _out_ready = true;
+        _parsed_command.back()._redirect_out_type = 2;
+    }
+}
+void command_sequence::reset() {
+    _parsed_command.clear();
+    _command_relation.clear();
+    _in_ready = false; _out_ready = false;
+    _new_cmd = true; _new_ins = true;
+    _further_input = false;
+}
+};
---
+++ command.hpp
@@ -0,0 +1,57 @@
+#ifndef _ASP_COMMAND_HPP_
+#define _ASP_COMMAND_HPP_
+
+#include <vector>
+#include <string>
+
+#include <cassert>
+
+namespace asp {
+
+struct command;
+struct command_sequence;
+
+struct command {
+    std::vector<std::vector<std::string>> _ins;
+    std::string _redirect_in;
+    std::string _redirect_out;
+    short _redirect_out_type = 0; // 0 none, 1 create, 2 append
+    bool _background = false;
+
+    const std::vector<std::string>& main_ins() const {
+        if (_ins.empty()) exit(EXIT_FAILURE);
+        return _ins.back();
+    }
+    void append_word(const std::string& _s) {
+        if (_ins.empty()) exit(EXIT_FAILURE);
+        _ins.back().emplace_back(_s);
+    }
+    void create_instruction() {
+        _ins.emplace_back();
+    }
+    size_t size() const { return _ins.size(); }
+};
+
+struct command_sequence {
+    std::vector<command> _parsed_command;
+    std::vector<std::string> _command_relation;
+
+    void build_word(const std::string& _s);
+    void build_instruction();
+    void build_command(const std::string& _relation, bool _bg = false);
+    void prepare_redirection(const std::string& _redir);
+    size_t size() const { return _parsed_command.size(); }
+    void ready_in() { _in_ready = true; }
+    void ready_out() { _out_ready = true; }
+    bool further_input() const { return _further_input; }
+    void need_further_input() { _further_input = true; }
+    void reset();
+private:
+    bool _in_ready = false; bool _out_ready = false;
+    bool _new_ins = true; bool _new_cmd = true;
+    bool _further_input = false;
+};
+
+};
+
+#endif // _ASP_COMMAND_HPP_
